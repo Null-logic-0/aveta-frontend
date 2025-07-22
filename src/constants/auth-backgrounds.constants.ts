8e0a0f46b86129e221172bb616f20a9e70608545
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2025-07-22 21:44:10 +0400

    Add new auth background images and update constants
    Added images 7-12 to the bg-images directory and updated auth-backgrounds.constants.ts to include these new images in the AUTH_BACKGROUND_IMAGES array. This expands the set of backgrounds available for authentication screens.

--- src/constants/auth-backgrounds.constants.ts
+++ src/constants/auth-backgrounds.constants.ts
@@ -4,6 +4,25 @@ import img3 from "../assets/bg-images/img-3.jpg";
 import img4 from "../assets/bg-images/img-4.jpg";
 import img5 from "../assets/bg-images/img-5.jpg";
 import img6 from "../assets/bg-images/img-6.jpg";
+import img7 from "../assets/bg-images/img-7.jpg";
+import img8 from "../assets/bg-images/img-8.jpg";
+import img9 from "../assets/bg-images/img-9.jpg";
+import img10 from "../assets/bg-images/img-10.jpg";
+import img11 from "../assets/bg-images/img-11.jpg";
+import img12 from "../assets/bg-images/img-12.jpg";
 
-export const AUTH_BACKGROUND_IMAGES = [img1, img2, img3, img4, img5, img6];
+export const AUTH_BACKGROUND_IMAGES = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+];
 export const IMAGE_CHANGE_INTERVAL_MS = 120000;
