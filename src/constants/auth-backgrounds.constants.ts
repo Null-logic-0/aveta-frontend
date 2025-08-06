89ba4c6045a1c6ee3a099f5c02d739e57b2df802
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2025-08-06 17:18:58 +0400

    Add rollup-plugin-visualizer and dynamic image imports
    Added rollup-plugin-visualizer to devDependencies and integrated it into the Vite config for bundle analysis. Refactored AUTH_BACKGROUND_IMAGES to use dynamic imports via URL for better maintainability. Added stats.html for bundle visualization output.

--- src/constants/auth-backgrounds.constants.ts
+++ src/constants/auth-backgrounds.constants.ts
@@ -1,28 +1,20 @@
-import img1 from "../assets/bg-images/img-1.jpg";
-import img2 from "../assets/bg-images/img-2.jpg";
-import img3 from "../assets/bg-images/img-3.jpg";
-import img4 from "../assets/bg-images/img-4.jpg";
-import img5 from "../assets/bg-images/img-5.jpg";
-import img6 from "../assets/bg-images/img-6.jpg";
-import img7 from "../assets/bg-images/img-7.jpg";
-import img8 from "../assets/bg-images/img-8.jpg";
-import img9 from "../assets/bg-images/img-9.jpg";
-import img10 from "../assets/bg-images/img-10.jpg";
-import img11 from "../assets/bg-images/img-11.jpg";
-import img12 from "../assets/bg-images/img-12.jpg";
-
-export const AUTH_BACKGROUND_IMAGES = [
-  img1,
-  img2,
-  img3,
-  img4,
-  img5,
-  img6,
-  img7,
-  img8,
-  img9,
-  img10,
-  img11,
-  img12,
+const imagePaths = [
+  "img-1.jpg",
+  "img-2.jpg",
+  "img-3.jpg",
+  "img-4.jpg",
+  "img-5.jpg",
+  "img-6.jpg",
+  "img-7.jpg",
+  "img-8.jpg",
+  "img-9.jpg",
+  "img-10.jpg",
+  "img-11.jpg",
+  "img-12.jpg",
 ];
+
+export const AUTH_BACKGROUND_IMAGES = imagePaths.map(
+  (img) => new URL(`../assets/bg-images/${img}`, import.meta.url).href
+);
+
 export const IMAGE_CHANGE_INTERVAL_MS = 120000;
