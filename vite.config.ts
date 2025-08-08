0907b20d6effa1f693f3a051abdfd1b6610244df
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2025-08-08 12:27:11 +0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,4 @@ import { visualizer } from "rollup-plugin-visualizer";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), visualizer()],
-  server: {
-    proxy: {
-      "/": {
-        target: "http://ec2-13-218-165-221.compute-1.amazonaws.com",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
